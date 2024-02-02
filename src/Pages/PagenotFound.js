32f4d08980c908c875d161fc3fb64708ada4c7b5
Shahzaib <shaulazain@gmail.com>
2024-02-03 01:57:01 +0500

    initial commit

---
+++ src/Pages/PagenotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../Components/layout/Layout'
+
+const PagenotFound = () => {
+  return (
+    <Layout>PagenotFound</Layout>
+  )
+}
+
+export default PagenotFound
